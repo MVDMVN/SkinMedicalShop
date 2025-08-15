61ce59185ebd38bba6937122feb906f8ebd86727
MVDMVN <edifens@gmail.com>
2025-08-15 09:34:26 +0300

    small fixes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
+  new Splide("#hero-slider", {
+    type: "loop",
+    autoplay: true,
+    interval: 3000,
+    pauseOnHover: false,
+    arrows: true,
+    pagination: true,
+    speed: 800,
+    perMove: 1,
+  }).mount();
+
+  new Splide("#popular-slider", {
+    type: "loop",
+    perMove: 1,
+    perPage: 4,
+    gap: "40px",
+    pagination: true,
+    arrows: false,
+    breakpoints: {
+      1024: { perPage: 3 },
+      768: { perPage: 2 },
+      480: { perPage: 1 },
+    },
+
+    breakpoints: {
+      768: {
+        perPage: 2,
+        gap: "12px",
+      },
+    },
+  }).mount();
+
   new Splide("#rec-splide", {
     type: "loop",
     width: "850px",
@@ -12,6 +44,27 @@ document.addEventListener("DOMContentLoaded", () => {
       1024: { perPage: 3, gap: "20px" },
       768: { perPage: 1.5, gap: "20px" },
       0: { perPage: 1.5, gap: "16px" },
+    },
+  }).mount();
+
+  new Splide("#discount-slider", {
+    type: "loop",
+    perMove: 1,
+    perPage: 4,
+    gap: "40px",
+    pagination: true,
+    arrows: false,
+    breakpoints: {
+      1024: { perPage: 3 },
+      768: { perPage: 2 },
+      480: { perPage: 1 },
+    },
+
+    breakpoints: {
+      768: {
+        perPage: 2,
+        gap: "12px",
+      },
     },
   }).mount();
 
